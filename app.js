35ffc689d5a989150c039aef84760ee310dc5ddf
arman <md.arman2806@gmail.com>
2022-07-16 11:23:13 +0600

    implement localstorage, generate function component, load data from localstorage

--- app.js
+++ app.js
@@ -2,32 +2,47 @@ let input = document.getElementById("input-Box");
 let btn = document.getElementById("button");
 let list = document.getElementById("list");
 let err = document.getElementById("err");
+let showData = document.getElementById('showData');
 
+let todoItems = localStorage.getItem("todo_items");
+let arr = [];
+if (todoItems) {
+    arr = JSON.parse(todoItems);
+    arr.forEach(addTodoItem);
+}
 
 btn.addEventListener("click", function () {
-    if (input.value === "") {
+    const inputValue = input.value;
+    if (inputValue === "") {
         err.innerHTML = "fill the input box";
     }
 
-    if (input.value) {
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        li.className = "list-item";
+    if (inputValue) {
+        addTodoItem(inputValue);
+        arr.push(inputValue);
 
-        list.insertAdjacentElement('afterbegin', li);
-
-
-        let deleteItem = document.createElement("a");
-        deleteItem.href = "javascript:void(0)";
-        deleteItem.innerHTML = "x";
-        deleteItem.className = 'delete-item-btn';
-        li.appendChild(deleteItem);
-
-        deleteItem.addEventListener('click', function () {
-            li.remove()
-        })
-
+        localStorage.setItem("todo_items", JSON.stringify(arr));
         err.innerHTML = "";
     }
     input.value = "";
 });
+
+function appendDeleteButton(li){
+    let deleteItem = document.createElement("a");
+    deleteItem.href = "javascript:void(0)";
+    deleteItem.innerHTML = "x";
+    deleteItem.className = 'delete-item-btn';
+    li.appendChild(deleteItem);
+    deleteItem.addEventListener('click', function () {
+        li.remove()
+    })
+}
+
+function addTodoItem(todoValue) {
+    let li = document.createElement("li");
+    li.innerHTML = todoValue;
+    li.className = "list-item";
+
+    list.insertAdjacentElement('afterbegin', li);
+    appendDeleteButton(li);
+}
